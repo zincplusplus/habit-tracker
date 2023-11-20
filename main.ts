dc23f3719245e94b93457030462aa7c0c760f781
zoreet <zoreetdotcom@gmail.com>
2023-11-20 12:47:05 +0100

    TBH I'm not sure what I was doing. so I'll just save it

--- main.ts
+++ main.ts
@@ -5,6 +5,7 @@
 // example of replacing codeblocks https://github.com/joleaf/obsidian-email-block-plugin
 // https://ourgreenstory.com/nl/sticky-whiteboard/habit-tracker/
 
+/* eslint-disable */
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
@@ -57,7 +58,7 @@ export default class HabitTracker extends Plugin {
 				cls: 'habit_tracker'
 			});
 			root.addEventListener('click', e => {
-				if(e.target.classList.contains('habit-tick')) {
+				if(e.target?.classList.contains('habit-tick')) {
 					toggleHabit(
 						e.target,
 						e.target.getAttribute('habit'),
@@ -95,9 +96,10 @@ export default class HabitTracker extends Plugin {
 					cls: 'habit-tracker__row'
 				})
 
-				row.createEl('div', {
-					text: f['name'].replace('.md',''),
+				row.createEl('a', {
+					text: `${f['name']}`.replace('.md',''),
 					cls: 'habit-name habit-cell',
+					href: f.path,
 				})
 
 				let cell;
@@ -167,7 +169,7 @@ export default class HabitTracker extends Plugin {
 			Object.keys(fm).forEach(f => {
 				if(Array.isArray(fm[f])) {
 					let fmArray = `${f}:\n`;
-					fm[f].forEach(el => {
+					fm[f].forEach((el) => {
 						fmArray += `  - ${el}\n`
 					});
 					result +=fmArray;
@@ -181,142 +183,9 @@ export default class HabitTracker extends Plugin {
 			return result;
 		}
 
-		// async function toggleHabit(path,date,value) {
-		// 	const file = await oApp.vault.read(file);
-		// 	console.log(file);
-		// }
-
-
-
-
-
-
-		// const data = this.app.metadataCache.getFileCache(files[0])?.frontmatter.habitTracker;
-
-		// // console.log('data for', files[0].name, data);
-
-
-		// //codeblock
-		// this.registerMarkdownCodeBlockProcessor("habittracker", async (src, el, ctx) => {
-		// 	// console.log('this is the config', src);
-		// 	// console.log('this is the root el', el.innerHTML);
-		// 	// console.log('context', ctx);
-		// 	const button = el.createEl('button', {
-		// 		text: "write something"
-		// 	});
-
-		// 	button.addEventListener('click',() => {
-		// 		habbitTrackerToggle(files[0],'2023-11-15', 'valueee');
-		// 	})
-		// 	return null;
-		// });
-		// window.oApp = this.app;
-
-		// //write something
-		// window.habbitTrackerToggle = function(file ,date, value) {
-		// 	console.log('habbitTrackerToggle', file, date);
-		// 	oApp.vault.read(file).then((r:string) => {
-		// 		const fileContent = r.replace('---\n','').split('---\n')[1]
-		// 		const frontmatter = makeFrontmatter(oApp.metadataCache.getFileCache(file)?.frontmatter);
-
-		// 		oApp.vault.modify(
-		// 			file,
-		// 			makeFrontmatter(frontmatter + '\n' + fileContent
-		// 			)
-		// 	});
-		// }
-
-		// function makeFrontmatter(fm) {
-		// 	console.log('raw frontmatter', fm)
-		// 	let result = "---\n"
-		// 	Object.keys(fm).forEach(f => {
-		// 		if(Array.isArray(fm[f])) {
-		// 			let fmArray = `${f}:\n`;
-		// 			fm[f].forEach(el => {
-		// 				fmArray += `  - ${el}\n`
-		// 			});
-		// 			result +=fmArray;
-		// 		} else {
-		// 			result += `${f}: ${fm[f]}\n`;
-		// 		}
-		// 	});
-		// 	result+="---"
-
-		// 	console.log('rendered frontmatter', result)
-		// 	return result;
-		// }
-
-		// function makeFile(file,frontmatter,content) {
-
-		// }
 
 		return null;
 
-		// window.habitTracker = (container, calendarData) => {
-		// 	console.log('hello world', container, calendarData);
-		// }
-		// await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		// const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-		// 	// Called when the user clicks the icon.
-		// 	new Notice('This is a notice!');
-		// });
-		// Perform additional things with the ribbon
-		// ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		// const statusBarItemEl = this.addStatusBarItem();
-		// statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		// this.addCommand({
-		// 	id: 'open-sample-modal-simple',
-		// 	name: 'Open sample modal (simple)',
-		// 	callback: () => {
-		// 		new SampleModal(this.app).open();
-		// 	}
-		// });
-		// This adds an editor command that can perform some operation on the current editor instance
-		// this.addCommand({
-		// 	id: 'sample-editor-command',
-		// 	name: 'Sample editor command',
-		// 	editorCallback: (editor: Editor, view: MarkdownView) => {
-		// 		console.log(editor.getSelection());
-		// 		editor.replaceSelection('Sample Editor Command');
-		// 	}
-		// });
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		// this.addCommand({
-		// 	id: 'open-sample-modal-complex',
-		// 	name: 'Open sample modal (complex)',
-		// 	checkCallback: (checking: boolean) => {
-		// 		// Conditions to check
-		// 		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-		// 		if (markdownView) {
-		// 			// If checking is true, we're simply "checking" if the command can be run.
-		// 			// If checking is false, then we want to actually perform the operation.
-		// 			if (!checking) {
-		// 				new SampleModal(this.app).open();
-		// 			}
-
-		// 			// This command will only show up in Command Palette when the check function returns true
-		// 			return true;
-		// 		}
-		// 	}
-		// });
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		// this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-		// 	console.log('click', evt);
-		// });
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -331,22 +200,6 @@ export default class HabitTracker extends Plugin {
 		await this.saveData(this.settings);
 	}
 }
-
-// class SampleModal extends Modal {
-// 	constructor(app: App) {
-// 		super(app);
-// 	}
-
-// 	onOpen() {
-// 		const {contentEl} = this;
-// 		contentEl.setText('Woah!');
-// 	}
-
-// 	onClose() {
-// 		const {contentEl} = this;
-// 		contentEl.empty();
-// 	}
-// }
 
 // class SampleSettingTab extends PluginSettingTab {
 // 	plugin: MyPlugin;
@@ -373,3 +226,4 @@ export default class HabitTracker extends Plugin {
 // 				}));
 // 	}
 // }
+/* eslint-enable */
