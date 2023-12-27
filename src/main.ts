2457dde1e0dd1e87a8471692294c5fcd7ca55a38
zoreet <zoreetdotcom@gmail.com>
2023-12-27 07:20:39 +0100

    use tabs instead of spaces because https://www.reddit.com/r/javascript/comments/c8drjo/comment/eso1yta/

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ const PLUGIN_NAME = "Habit Tracker 21"
 
 export default class HabitTracker21 extends Plugin {
 
-  async onload() {
+	async onload() {
 		console.log(`${PLUGIN_NAME}: loading...`);
 		this.registerMarkdownCodeBlockProcessor("habittracker", async (src, el, ctx) => {
 			new HabitTracker(src, el, ctx, this.app);
 		});
-  }
+	}
 }
