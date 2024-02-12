c8db34fa91a8cb7ad7b7450e1812e7400cb5536f
Even Holen <Even.Holen@gmail.com>
2024-02-13 00:02:42 +0100

    #2 Use Style Settings to set CSS
    Here is a starter set for setting colors through the Style Settings plugin. Even if that plugin is not present, the variables have a default value which then will be used.

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ const PLUGIN_NAME = 'Habit Tracker 21'
 export default class HabitTracker21 extends Plugin {
 	async onload() {
 		console.log(`${PLUGIN_NAME}: loading...`)
+		this.app.workspace.trigger("parse-style-settings")
 		this.registerMarkdownCodeBlockProcessor(
 			'habittracker',
 			async (src, el, ctx) => {
