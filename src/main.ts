41c8f76030be81ef902cdeabc469ba2730e1c231
zoreet <zoreetdotcom@gmail.com>
2024-07-26 22:57:29 +0200

    1.4.3 - remove FE analytics

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,6 @@ export default class HabitTracker21 extends Plugin {
 		this.registerMarkdownCodeBlockProcessor(
 			'habittracker',
 			async (src, el, ctx) => {
-				// track if people are using this version
-				const bitly = document.createElement('img');
-				bitly.setAttribute('src', 'https://bit.ly/habitttracker21-142');
-				bitly.setAttribute("style", "height: 1px; width: 1px; border: none; opacity: 0; position: absolute; top: 0; left: 0;pointer-events: none;")
-				el?.parentElement.appendChild(bitly);
 				new HabitTracker(src, el, ctx, this.app)
 			},
 		)
